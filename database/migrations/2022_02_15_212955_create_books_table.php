23be27aa480c212570c14200f6b62eafb118c457
Adam Kos <kostecki.adam@gmail.com>
2022-02-15 22:58:47 +0100

    defining migration

---
+++ database/migrations/2022_02_15_212955_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('desctiption');
+            $table->string('author');
+            $table->integer('categories_id');
+            $table->integer('pages');
+            $table->string('date');
+            $table->string('photo');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
