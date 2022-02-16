7dfdf6d3e4c59d9f2e2d0ac18579d59b3619bee2
Adam Kos <kostecki.adam@gmail.com>
2022-02-16 21:41:57 +0100

    create home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,38 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-lg-3 col-md-3 col-sm-4">
+            menu
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-4">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="row">
+
+                <div class="col-lg-2 col-md-4 col-sm-6">
+                    <div class="card-deck">
+                        <div class="card">
+                            <img src="" class="card-img-top img-responsive" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Tytuł książki</h5>
+                                <p class="card-text">autor</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="test-muted">kategoria</small>
+                            </div>
+                            <div>&nbsp;</div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-succes btn-rounded btn-block">Szczegóły</a>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
+
             </div>
+            <div class="alert alert-icon alert-white alert-info alert-dismissble fade in" role="alert">
+                <i class="mdi mdi-infrmation"></i>
+                <strong>Przykro nam</strong>Obecnie nie posiadamy książek w naszych zbioirach
+            </div>
+
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', 'App\Http\Controllers\HomeController@index'); //<-- sprawdzić jak się robiło skrót 
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
